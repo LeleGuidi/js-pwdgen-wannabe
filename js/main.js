f7f505ac91b279736da899636ec4b8c731aac4e2
LeleGuidi <emanuele.guidi98@outlook.it>
2022-04-27 15:03:22 +0200

    Inseriti prompt per chiedere i dati all'utente

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+const FirstName = prompt("Qual'è il tuo nome?");
+console.debug(FirstName);
+const surname = prompt("Qual'è il tuo cognome?")
+console.debug(surname)
+const color = prompt("Qual'è il tuo colore preferito?")
+console.debug(color)
+
